e6db3a7b8a584ff08c98787cbae2c3e5b65304d0
hzc111 <77872849+hzc111@users.noreply.github.com>
2021-05-23 20:58:58 +0800

    Add files via upload

---
+++ cdn/js/hititle.js
@@ -0,0 +1,17 @@
+//标题恶搞
+var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '404 not found';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '嘿嘿,上当啦！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
