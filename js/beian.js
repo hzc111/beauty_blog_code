cafe1169a9c305785c97ef9f2f1f75161bac8d5a
hzc111 <77872849+hzc111@users.noreply.github.com>
2021-06-01 22:34:31 +0800

    Add files via upload

---
+++ js/beian.js
@@ -0,0 +1,18 @@
+<div class="foot_bot">
+   <font><a   href="javascript:void(0)" onclick="gxtz()" style="color:#4f4f4f;">闽ICP备2021007560号-1</a></font>   
+</div>
+ 
+<script>
+function gxtz(){
+    window.open('javascript:window.name;', '<script>location.replace("http://www.beian.miit.gov.cn/publish/query/indexFirst.action")<\/script>');
+ }
+</script>
+	
+<style>
+ 
+.foot_bot{width:1200px;margin:890px auto 0;overflow:hidden;}
+.foot_bot span{float:left;}
+.foot_bot span a{margin-right:5px;}
+.foot_bot span a:hover{background:#fff;filter: alpha(opacity=80); opacity: 0.8;}
+.foot_bot font{float:right;font-size:14px;color:#4f4f4f;margin-top:8px;}
+.systemlogo{width:100%; height:71px; margin-top:120px; text-align:center;}
